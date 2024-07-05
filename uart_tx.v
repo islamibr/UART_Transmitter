dbc386ae8ca929fd94ea068afd1920246b14405a
Islam Ibrahim <49861069+islamibr@users.noreply.github.com>
2024-07-05 22:19:50 +0300

    Update uart_tx.v

--- uart_tx.v
+++ uart_tx.v
@@ -1,26 +1,58 @@
 module uart_tx(
-	//DO NOT EDIT any part of this port declaration
-	input 		clock, rst, send,
-	input [1:0]	baud_rate,
-	input [7:0] data_in, 
-	input [1:0] parity_type, 	//refer to the block comment above. 
-	input 		stop_bits, 		//low when using 1 stop bit, high when using two stop bits
-	input 		data_length, 	//low when using 7 data bits, high when using 8.
-	
-	output reg 	data_out, 		//Serial data_out
-	output reg 	p_parity_out, 	//parallel odd parity output, low when using the frame parity.
-	output reg 	tx_active, 		//high when Tx is transmitting, low when idle.
-	output reg 	tx_done 		//high when transmission is done, low when not.
+    // DO NOT EDIT any part of this port declaration
+    input        clock, rst, send,
+    input [1:0]  baud_rate,
+    input [7:0]  data_in, 
+    input [1:0]  parity_type, // refer to the block comment above. 
+    input        stop_bits,   // low when using 1 stop bit, high when using two stop bits
+    input        data_length, // low when using 7 data bits, high when using 8.
+    
+    output       data_out,    // Serial data_out
+    output       p_parity_out,// parallel odd parity output, low when using the frame parity.
+    output       tx_active,   // high when Tx is transmitting, low when idle.
+    output       tx_done      // high when transmission is done, low when not.
 );
 
-	//You MAY EDIT these signals, or module instantiations.
-	reg parity_out, baud_out;
-	reg [10:0] frame_out;
-	
-	//sub_modules
-	parity		parity_gen1 (rst, data_in, parity_type, parity_out);
-	frame_gen	frame_gen1  (rst, data_in, parity_out, parity_type, stop_bits, data_length, frame_out);
-	baud_gen	baud_gen1	(rst, clock, baud_rate, baud_out);
-	piso		shift_reg1	(rst, frame_out, parity_type, stop_bits, data_length, send, baud_out, data_out, p_parity_out, tx_active, tx_done);
+    // You MAY EDIT these signals, or module instantiations.
+    wire parity_out, baud_out;
+    wire [10:0] frame_out;
+
+    // sub_modules
+    parity parity_gen1 (
+        .rst(rst),
+        .data_in(data_in),
+        .parity_type(parity_type),
+        .parity_out(parity_out)
+    );
+
+    frame_gen frame_gen1 (
+        .rst(rst),
+        .data_in(data_in),
+        .parity_out(parity_out),
+        .parity_type(parity_type),
+        .stop_bits(stop_bits),
+        .data_length(data_length),
+        .frame_out(frame_out)
+    );
+
+    baud_gen baud_gen1 (
+        .clock(clock),
+        .rst(rst),
+        .baud_rate(baud_rate),
+        .baud_out(baud_out)
+    );
+    piso shift_reg1 (
+        .rst(rst),
+        .frame_out(frame_out),
+        .parity_type(parity_type),
+        .stop_bits(stop_bits),
+        .data_length(data_length),
+        .send(send),
+        .baud_out(baud_out),
+        .data_out(data_out),
+        .p_parity_out(p_parity_out),
+        .tx_active(tx_active),
+        .tx_done(tx_done)
+    );
 
 endmodule
